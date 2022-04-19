2a4e64ecf58456cccf7dbfdbd2bca02161bf1d14
imteddy07 <teddyadityap07@gmail.com>
2022-04-19 20:09:26 +0700

    Update-Delete

--- src/test/java/api/Reqresin.java
+++ src/test/java/api/Reqresin.java
@@ -7,45 +7,6 @@ import org.junit.Assert;
 
 public class Reqresin {
     private static final String url = "https://reqres.in";
-
-    //Scenario GET Single user
-    public void GetSingleUser() {
-        SerenityRest.get(url + "/api/users/2");
-    }
-
-    public void UserNotFound() {
-        SerenityRest.get(url + "/api/users/23");
-    }
-    public void PostLogin () {
-        JSONObject bodyJson = new JSONObject();
-        bodyJson.put("email", "eve.holt@reqres.in");
-        bodyJson.put("password", "cityslicka");
-
-        SerenityRest.given()
-                .header("Content-type", "application/json")
-                .body(bodyJson.toString())
-                .post(url + "/api/login");
-    }
-    public void PostRegister () {
-        JSONObject bodyJson = new JSONObject();
-        bodyJson.put ("email", "eve.holt@reqres.in");
-        bodyJson.put ("password", "pistol");
-
-        SerenityRest.given()
-                .header("Content-type", "application/json")
-                .body(bodyJson.toString())
-                .post(url + "/api/register");
-    }
-    public void PostCreate () {
-        JSONObject bodyJson = new JSONObject();
-        bodyJson.put ("name", "morpheus");
-        bodyJson.put ("job", "leader");
-
-        SerenityRest.given()
-                .header("Content-type", "application/json")
-                .body(bodyJson.toString())
-                .post(url + "/api/users");
-    }
     public void PutUpdate() {
         JSONObject bodyJson = new JSONObject();
         bodyJson.put ("name", "morpheus");
--- src/test/java/stepdef/MyStepdefs.java
+++ src/test/java/stepdef/MyStepdefs.java
@@ -11,11 +11,6 @@ import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 public class MyStepdefs {
     Reqresin reqresin = new Reqresin();
 
-    @When("User send GET HTTP request to reqresin")
-    public void userSendGETHTTPRequestToReqresin() {
-        reqresin.GetSingleUser();
-    }
-
     @Then("response status code should be {int}")
     public void responseStatusCodeShouldBe (int statusCode) {
         restAssuredThat(response -> response.statusCode(statusCode));
@@ -27,20 +22,6 @@ public class MyStepdefs {
             restAssuredThat(response -> response.assertThat().body(matchesJsonSchemaInClasspath(path)));
     }
 
-    @When("User send other GET HTTP requset to reqresin")
-    public void userSendOtherGETHTTPRequsetToReqresin() {
-        reqresin.UserNotFound();
-    }
-
-    @When("User send POST register HTTP request to reqresin")
-    public void userSendPOSTRegisterHTTPRequestToReqresin() {
-        reqresin.PostRegister();
-    }
-
-    @When("User send POST create HTTP request to reqresin")
-    public void userSendPOSTCreateHTTPRequestToReqresin() {
-        reqresin.PostCreate();
-    }
 
     @When("User send PUT update HTTP request to reqresin")
     public void userSendPUTUpdateHTTPRequestToReqresin() {
@@ -52,10 +33,6 @@ public class MyStepdefs {
         reqresin.DeleteData();
     }
 
-    @When("User send POST login HTTP request to reqresin")
-    public void userSendPOSTLoginHTTPRequestToReqresin() {
-        reqresin.PostLogin();
-    }
 
 
 }
